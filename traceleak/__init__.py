199ab506864d2fa0c39ebebc42545e9c979d5748
Unjuno <unjuno.org@unjuno.org>
2026-06-10 17:18:03 +0900

    core: expose selected public APIs

--- traceleak/__init__.py
+++ traceleak/__init__.py
@@ -1,6 +1,25 @@
 """TraceLeak: source-level leakage assessment primitives."""
 
-from traceleak.metrics import delta_h
+from traceleak.attribution import AttributionScore, ablation_drop
+from traceleak.config import validate_config
+from traceleak.features import extract_feature_vector
+from traceleak.metrics import accuracy, delta_h, top_k_recall
 from traceleak.schema import TraceEvent, TraceRun, validate_run
+from traceleak.views import to_view
+from traceleak.workflow import WorkflowResult, run_lightweight_experiment
 
-__all__ = ["TraceEvent", "TraceRun", "delta_h", "validate_run"]
+__all__ = [
+    "AttributionScore",
+    "TraceEvent",
+    "TraceRun",
+    "WorkflowResult",
+    "ablation_drop",
+    "accuracy",
+    "delta_h",
+    "extract_feature_vector",
+    "run_lightweight_experiment",
+    "to_view",
+    "top_k_recall",
+    "validate_config",
+    "validate_run",
+]
